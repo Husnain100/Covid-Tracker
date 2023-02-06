ca180bb517aa970ca86aea784d121ea847c4518a
Husnain <husnain.bajwa@capgemini.com>
2023-02-06 01:42:43 +0000

    adding in material UI and form control

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,8 +1,31 @@
+import { MenuItem, Select } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <h1>Covid-19 Tracker</h1>
+
+      <FormControl className='app__dropdown'>
+        <Select variant='outlined' value={'abc'}>
+          <MenuItem value='Wrldwide'>WorldWide</MenuItem>
+        </Select>
+      </FormControl>
+      {/* SCREEN GOES BLANK WHEN FORM CONTROL IS USED. FIX THIS */}
+
+      {/*HEADER*/}
+      {/*TITLE + SELECT INPUT DROPDOWN FIELD*/}
+
+      {/*INFO BOXES*/}
+      {/*INFO BOXES*/}
+      {/*INFO BOXES*/}
+
+      {/*TABLE*/}
+      {/*GRAPH*/}
+
+      {/*MAP*/}
+
 
     </div>
   );
